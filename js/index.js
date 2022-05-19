1de6866ccc5903705445399578d0927d1cbbda66
timurkarshiev <timurlan.grozniy@gmail.com>
2022-05-19 23:49:46 +0500

    init: end hw

---
+++ js/index.js
@@ -0,0 +1,17 @@
+var name = prompt("❕Ismingizni kiriting❕", "Timur");
+var surname = prompt("❕Familiyangizni kiriting❕", "Karshiev");
+var from = prompt("🏙Qayerdansiz?🏙", "Samarkand");
+var wb = prompt("📅Nechinchi yilda tug'ilgansiz?📅", "1999");
+var number = prompt("🤴Oilada nechtasizlar?👱‍♀️", "77777777");
+var married = prompt("🤵👰Turmush qurganmisiz?🤵👰", "Startda");
+var workorstudy = prompt("👨‍🎓👨‍🔧O'qiysizmi yoki ishlaysizmi?👨‍🎓👨‍🔧", "O'qiyman");
+var wherews = prompt("👨‍🚀👨‍💻Qayerda o'qiysiz yoki ishlaysiz?👨‍🚀👨‍💻", "University");
+var future = prompt("🙋‍♂️Kelajakda kim bo'lmoxchisiz?🧏‍♂️", "Software Developer");
+var interest = prompt("💵1mln pulingiz bo'lsa nima qilardingiz?💵", "Hard sotib olardim");
+
+
+
+
+alert (name + " " + surname + "\n" + from + "\n" + wb + "\n" + number + "\n" + married + "\n" + workorstudy + "\n" + wherews + "\n" + future + "\n" + interest);
+
+console.log(name + " " + surname + "\n" + from + "\n" + wb + "\n" + number + "\n" + married + "\n" + workorstudy + "\n" + wherews + "\n" + future + "\n" + interest);
